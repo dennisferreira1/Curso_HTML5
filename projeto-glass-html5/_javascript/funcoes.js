c47525330385bd83d51309ef0d58728edcb3b767
dennisferreira1 <denniferreira1@hotmail.com>
2020-04-21 21:41:23 -0300

    Incluindo função em funcoes.js para listar cidades por estado
    Incluindo função em funcoes.js e fazendo a chamada em fale-conosco.html

--- projeto-glass-html5/_javascript/funcoes.js
+++ projeto-glass-html5/_javascript/funcoes.js
@@ -1,3 +1,20 @@
 function mudaFotoIcone(foto) {
     document.getElementById("icone").src = foto;
 }
+
+function listaCidades(estado){
+    var cidadesPorEstados = {RN:["Natal", "Parnamirim", "Macaíba", "São Gonçalo", "Extremoz", "Ceará-Mirim"],
+                            CE:["Fortaleza"], PB:["João Pessoa"], PE:["Recife"], RJ:["Rio de Janeiro"],
+                            SP:["São Paulo"], SC:["Florianópolis"], RS:["Porto Alegre"], MG:["Belo Horizonte"]
+                            }
+
+    var options = '';
+    
+    if(cidadesPorEstados.hasOwnProperty(estado)){
+        for(var i=0; i<cidadesPorEstados[estado].length; i++){
+            options += '<option value= "' + cidadesPorEstados[estado][i] + '"/>';
+        }
+    }
+
+    document.getElementById("listCidades").innerHTML = options;
+}
